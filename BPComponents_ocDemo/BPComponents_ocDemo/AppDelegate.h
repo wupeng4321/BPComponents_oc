a066725cf71376b0970c329e64ed13cec055a288
18092515 <18092515@cnsuning.com>
2020-12-14 12:55:10 +0800

    init

---
+++ BPComponents_ocDemo/BPComponents_ocDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BPComponents_ocDemo
+//
+//  Created by wupeng on 2020/12/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BPComponents_ocDemo/BPComponents_ocDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  BPComponents_ocDemo
+//
+//  Created by wupeng on 2020/12/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
