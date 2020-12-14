3c9a59171782295d031037f357ad90967609ae06
wupeng4321 <wupeng_4321@163.com>
2020-12-14 13:10:40 +0800

    init

---
+++ BPComponents_oc/BPComponents_oc/BPComponents_oc.h
@@ -0,0 +1,18 @@
+//
+//  BPComponents_oc.h
+//  BPComponents_oc
+//
+//  Created by wupeng on 2020/12/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BPComponents_oc.
+FOUNDATION_EXPORT double BPComponents_ocVersionNumber;
+
+//! Project version string for BPComponents_oc.
+FOUNDATION_EXPORT const unsigned char BPComponents_ocVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BPComponents_oc/PublicHeader.h>
+
+
---
+++ BPComponents_ocDemo/Pods/Target Support Files/Pods-BPComponents_ocDemo/Pods-BPComponents_ocDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BPComponents_ocDemo : NSObject
+@end
+@implementation PodsDummy_Pods_BPComponents_ocDemo
+@end
